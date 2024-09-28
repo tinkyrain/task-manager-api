8c74f6c9f55b8a4694d4cf7600b8d64c829187fb
Svetlovskii Vlad <v.svetlovskiy@roiburo.ru>
2024-09-28 18:20:44 +0500

    add error json handler

---
+++ src/Handlers/Errors/JsonErrorHandler.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Handlers\Errors;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Slim\Exception\HttpException;
+use Slim\Exception\HttpMethodNotAllowedException;
+use Slim\Psr7\Response;
+
+class JsonErrorHandler
+{
+    public function __invoke(ServerRequestInterface $request, \Throwable $exception): ResponseInterface
+    {
+        $response = new Response();
+        $data = [
+            'success' => false,
+            'message' => $exception->getMessage(),
+            'status_code' => $exception->getCode(),
+        ];
+
+        // Установить код состояния в зависимости от типа исключения
+        if ($exception instanceof HttpException) {
+            $statusCode = $exception->getCode();
+        } elseif ($exception instanceof HttpMethodNotAllowedException) {
+            $statusCode = 405; // Method not allowed
+        } else {
+            $statusCode = 500; // Server error
+        }
+
+        $response = $response->withStatus($statusCode);
+        $response->getBody()->write(json_encode($data));
+        return $response->withHeader('Content-Type', 'application/json');
+    }
+}
