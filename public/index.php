86721e7ae6aafd37ceb6ff09d4c6582863c43c7e
Svetlovskii Vlad <v.svetlovskiy@roiburo.ru>
2024-09-29 12:35:06 +0500

    add bootstrap file

--- public/index.php
+++ public/index.php
@@ -1,24 +1,6 @@
 <?php
 
-use App\Handlers\Errors\JsonErrorHandler;
-use Slim\Factory\AppFactory;
+require_once '../config/bootstrap.php';
 
-require __DIR__ . '/../vendor/autoload.php'; //include autoload
-
-//create app
-$app = AppFactory::create();
-
-//region Middlewares
-$app->addErrorMiddleware(true, true, true)
-    ->setDefaultErrorHandler(new JsonErrorHandler());
-//endregion
-
-//region ReadBean ORM
-require __DIR__ . '/../config/orm/rb.php';
-//endregion
-
-//region Routes
-(require __DIR__ . '/../routes/routes.php')($app);
-//endregion
-
+$app = createApp();
 $app->run();
